8211a8b427e7495a4f3cfe2908722246d37cb2b9
jensen xu <jensenxu@jensens-MacBook-Air.local>
2019-11-15 21:12:46 -0800

    q

--- try.R
+++ try.R
@@ -2,3 +2,7 @@
 d <- rnorm(n = 150, mean = 10, sd = 1.2)
 k <- rnorm(n = 150, mean = 7, sd = 3)
 plot(d ~ k)
+
+library(TSA)
+dd <- arima.sim(list(ar = c(0.5, 0.3), ma = c(0.4)), n = 100, sd = 2)
+pacf(dd)
